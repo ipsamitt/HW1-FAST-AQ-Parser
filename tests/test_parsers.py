c834bd47ece83c70ea881dd4d4164f9b36a3d4c4
ipsamitt <ipsam001@gmail.com>
2025-01-15 23:28:31 -0800

    edited test_parser

--- tests/test_parsers.py
+++ tests/test_parsers.py
@@ -24,39 +24,77 @@ def test_freebie_parser_2():
 
         
 def test_FastaParser():
-    fasta_parse_test = FastaParser("./blank.fa")
-  #  assert fasta_parse_test.get_record(f_obj=)
-    """
-    Write your unit test for your FastaParser class here. You should generate
-    an instance of your FastaParser class and assert that it properly reads in
-    the example Fasta File.
+    # Read in good fasta file 
+    fasta_file = 'data/test.fa'
+    parser_obj = FastaParser(fasta_file)
+    file_lines = [record for record in parser_obj]
 
-    Some example of "good" test cases might be handling edge cases, like Fasta
-    files that are blank or corrupted in some way. Two example Fasta files are
-    provided in /tests/bad.fa and /tests/empty.fa
-    """
-    pass
+    # known seq0 of fasta file
+    seq0 = 'TGATTGAATCTTTTGAGGGTCACGGCCCGGAAGCCAGAATTTCGGGGTCCTCTGTGGATATTAATCGAGCCCACACGGTGTGAGTTCAGCGGCCCCCGCA'
+    assert file_lines[0][1] == seq0, "Something is wrong - panic!"
 
+    # Read in blank fasta file 
+    fasta_file = 'tests/blank.fa'
+    parser_obj = FastaParser(fasta_file)
+    try:
+        file_lines = [record for record in parser_obj]
+    except ValueError:
+        assert True
+
+    # Read in bad fasta file 
+    fasta_file = 'tests/bad.fa'
+    parser_obj = FastaParser(fasta_file)
+    try:
+        file_lines = [record for record in parser_obj]
+    except ValueError:
+        assert True
 
 def test_FastaFormat():
     """
     Test to make sure that a fasta file is being read in if a fastq file is
     read, the first item is None
     """
-    pass
+
+    fasta_file = 'data/test.fq'
+    parser_obj = FastaParser(fasta_file)
+    file_lines = [record for record in parser_obj]
+
+    assert file_lines[0][0] == None, "Something is wrong - panic!"
+
 
 
 def test_FastqParser():
     """
-    Write your unit test for your FastqParser class here. You should generate
-    an instance of your FastqParser class and assert that it properly reads 
-    in the example Fastq File.
+    Write your unit test for your FastqParser
+    class here. You should generate an instance of
+    your FastqParser class and assert that it properly
+    reads in the example Fastq File.
     """
-    pass
+    # Read in fastq file
+    fastq_file = 'data/test.fq'
+    parser_obj = FastqParser(fastq_file)
+    file_lines = [record for record in parser_obj]
+
+    # Known seq0 of fastq file
+    seq0 = 'TGTGGTCGTATAGTTATTGTCATAAATTACACAGAATCGCGATTCTCCGCGTCCACCAATCTTAGTGCACCACAGCATCGACCCGATTTATGACGCTGAG'
+    # Known quality of seq0
+    qual0 = '*540($=*,=.062565,2>\'487\')!:&&6=,6,*7>:&132&83*8(58&59>\'8!;28<94,0*;*.94**:9+7"94(>7=\'(!5"2/!%"4#32='
+
+    # Check both sequence and quality
+    check_val = False
+    if ((file_lines[0][1] == seq0) and (file_lines[0][2] == qual0)):
+        check_val = True
+    
+    assert check_val, "FastQ parser error"
 
 def test_FastqFormat():
     """
     Test to make sure fastq file is being read in. If this is a fasta file, the
     first line is None
     """
-    pass
+    
+    fastq_file = 'data/test.fa'
+    parser_obj = FastqParser(fastq_file)
+    file_lines = [record for record in parser_obj]
+
+    assert file_lines[0][0] == None, "Something is wrong - panic!"
